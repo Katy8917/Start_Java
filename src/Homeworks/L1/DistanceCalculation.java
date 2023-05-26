59b77dbdd57d0807ed61a036fa7c3c3098756efa
Katy8917 <palii_ecaterina@mail.ru>
2023-05-26 21:17:09 +0300

    Added DistanceCalculation.java

--- src/Homeworks/L1/DistanceCalculation.java
+++ src/Homeworks/L1/DistanceCalculation.java
@@ -1,4 +1,13 @@
 package Homeworks.L1;
 
+import java.util.Scanner;
+
 public class DistanceCalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество галлонов");
+        double gallons = sc.nextDouble();
+        double liters = gallons * 3.7854;
+        System.out.println(gallons + "галлонам соответствует" + liters + "литров");
+    }
 }
