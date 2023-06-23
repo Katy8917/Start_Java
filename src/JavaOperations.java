d1f257a3148c9c049be2f37cea1403f6c13224fc
Katy8917 <palii_ecaterina@mail.ru>
2023-06-23 14:39:10 +0300

    Упражнение 4.2, со стр.154

--- src/JavaOperations.java
+++
@@ -1,9 +0,0 @@
-public class JavaOperations {
-    public static void main(String[] args) {
-
-        int a = 1, b = 1;
-
-        double resultPlus = a + b;
-        double resultMinus = a - b;
-    }
-}
